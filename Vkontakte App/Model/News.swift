9818d9d6515a72064b31eb3517578a91765f9ddb
domenada <adventurersuper@gmail.com>
2019-08-25 21:28:05 -0400

    Lesson 6

---
+++ Vkontakte App/Model/News.swift
@@ -0,0 +1,9 @@
+//
+//  News.swift
+//  Vkontakte App
+//
+//  Created by Dmitry Andreyanov on 8/23/19.
+//  Copyright © 2019 light. All rights reserved.
+//
+
+import Foundation
---
+++ Vkontakte App/Model/NewsControlkker+UITableViewDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  NewsControlkker+UITableViewDataSource.swift
+//  Vkontakte App
+//
+//  Created by Dmitry Andreyanov on 8/23/19.
+//  Copyright © 2019 light. All rights reserved.
+//
+
+import Foundation
